e910c58c4cbd511b8a3d03d6097ff2974c3a168f
Dastan <dastan.ertajly@mail.ru>
2026-09-01 19:16:04 +0500

    тест

--- script.js
+++ script.js
@@ -1,12 +1,85 @@
-let isStudent = true;
-let grade = 3;
+// Задание 1
+let number1 = Number(prompt("Задание 1: Введите число:"));
 
-if (isStudent) {
-   if (grade > 2) {
-       console.log("Вы успешно прошли курс.");
-   } else {
-       console.log("Вы не прошли курс.");
-   }
+if (number1 > 10) {
+    console.log("Число больше 10");
 } else {
-   console.log("Вы не являетесь студентом.");
-};
+    console.log("Число меньше или равно 10");
+}
+
+
+// Задание 2
+let answer = confirm("Задание 2: Вы хотите удалить файл?");
+
+if (answer) {
+    console.log("Файл удален");
+} else {
+    console.log("Удаление отменено");
+}
+
+
+// Задание 3
+let age = Number(prompt("Задание 3: Введите ваш возраст:"));
+
+if (age < 18) {
+    console.log("Вы еще подросток");
+} else if (age <= 30) {
+    console.log("Вы молодой взрослый");
+} else {
+    console.log("Вы взрослый");
+}
+
+
+// Задание 4
+let number2 = Number(prompt("Задание 4: Введите число:"));
+
+let evenOrOdd = number2 % 2 === 0
+    ? "Четное число"
+    : "Нечетное число";
+
+console.log(evenOrOdd);
+
+
+// Задание 5
+let day = Number(prompt("Задание 5: Введите число от 1 до 7:"));
+
+switch (day) {
+    case 1:
+        console.log("Понедельник");
+        break;
+    case 2:
+        console.log("Вторник");
+        break;
+    case 3:
+        console.log("Среда");
+        break;
+    case 4:
+        console.log("Четверг");
+        break;
+    case 5:
+        console.log("Пятница");
+        break;
+    case 6:
+        console.log("Суббота");
+        break;
+    case 7:
+        console.log("Воскресенье");
+        break;
+    default:
+        console.log("Некорректное значение");
+}
+
+
+// Задание 6
+let first = Number(prompt("Задание 6: Введите первое число:"));
+let second = Number(prompt("Введите второе число:"));
+
+if (first === second) {
+    console.log("Числа равны");
+} else {
+    let result = first > second
+        ? "Первое число больше"
+        : "Второе число больше";
+
+    console.log(result);
+}
